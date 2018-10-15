98156b8d34daef8f285e4e33ba85f3c7a3bc0a31
Jose Arauco <araucovillar@gmail.com>
2018-10-14 23:08:34 -0500

    dml changes

--- MiniEncuestaScript-DML.sql
+++ MiniEncuestaScript-DML.sql
@@ -1,1 +1,14 @@
-##CARGA INICIAL ####
+----------------------------------------------------------------------------------------------------------------------------------------
+-- CARGA INICIAL --
+----------------------------------------------------------------------------------------------------------------------------------------
+
+--DELETE
+DELETE me_encuesta_alternativa;
+DELETE me_encuesta_tema;
+
+--INSERT
+INSERT INTO `me_encuesta_tema` (`ente_codigo`, `ente_tema`, `ente_detalles`, `ente_estado`) VALUES ('FRUTA_FAV', 'Fruta Favorita', NULL, 'A');
+INSERT INTO `me_encuesta_tema` (`ente_codigo`, `ente_tema`, `ente_detalles`, `ente_estado`) VALUES ('LP_FAVORIT', 'Lenguaje de Programación favorito', NULL, 'A');
+
+INSERT INTO `me_encuesta_alternativa` (`enalt_id`, `ente_codigo`, `enalt_opcion`, `enalt_estado`) VALUES (1, 'LP_FAVORIT', 'JAVA', 'A');
+INSERT INTO `me_encuesta_alternativa` (`enalt_id`, `ente_codigo`, `enalt_opcion`, `enalt_estado`) VALUES (2, 'LP_FAVORIT', 'C#', 'A');
